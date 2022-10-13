e8f3015af047a20e007d47bb14f20c2f6475bfe9
SantiagoCastellani <monitocastellani@gmail.com>
2022-10-13 10:54:25 -0300

    ejercicio 2

---
+++ 2-Variables/index.js
@@ -0,0 +1,12 @@
+const nombre = "Santi"
+const edad = 37
+const isDeveloper = true
+const fechaNac = new Date(1985,03,24)
+const libroFavorito = {
+    tiulo:"El misterio cuántico",
+    autor:"Eric Zipronxs",
+    fecha: new Date(2007,05,15),
+    url:"https://open-bootcamp.com/"
+}
+
+const lista = [nombre,edad,isDeveloper,fechaNac,libroFavorito]
